e843db748855a5b52298d39b2863581a6df33978
Najib Ishaq <najib_ishaq@zoho.com>
2024-10-30 14:39:17 -0400

    wip: added calculations for finding spikes in entropy values

--- transforms/autocropping-tool/src/polus/images/transforms/autocropping/__init__.py
+++ transforms/autocropping-tool/src/polus/images/transforms/autocropping/__init__.py
@@ -3,9 +3,11 @@
 __version__ = "2.0.0"
 
 from . import entropy
+from . import gradients
 from .autocropping import autocropping
 
 __all__ = [
     "autocropping",
     "entropy",
+    "gradients",
 ]
---
+++ transforms/autocropping-tool/src/polus/images/transforms/autocropping/gradients.py
@@ -0,0 +1,37 @@
+"""Helpers for calculating gradients and spikes in a sequence of entropy values."""
+
+import numpy
+
+
+def find_spike_indices(data: numpy.ndarray, threshold: float) -> tuple[int, int]:
+    """Find the indices of the first and last spikes in a sequence of entropy values.
+
+    Args:
+        data: A numpy array (1D) representing a sequence of entropy values.
+        threshold: A float (positive) representing the threshold for the gradient.
+
+    Returns:
+        A tuple of two integers, the first representing the index of the start
+        of the first spike and the second representing the index of the end of
+        the last spike.
+    """
+    first_spike = find_spike(data, threshold)
+    last_spike = find_spike(data[::-1], threshold)
+    return first_spike, len(data) - last_spike - 1
+
+
+def find_spike(data: numpy.ndarray, threshold: float) -> int:
+    """Return the index of the start of the first spike in the data.
+
+    Args:
+        data: A numpy array (1D) representing a sequence of entropy values.
+        threshold: A float (positive) representing the threshold for the gradient.
+
+    Returns:
+        An integer representing the index of the start of the first spike.
+    """
+    # We expect gradients to be nearly zero before the spike and large when the
+    # spike starts
+
+    # Find the first spike
+    return numpy.argmax(numpy.abs(numpy.gradient(data)) > threshold)
